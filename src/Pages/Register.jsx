2fd44da336a3873131a85eff5a0f63ecfde4d8ca
patraomsai38 <patraomsai38@gmail.com>
2026-08-06 02:37:52 +0530

    fix: update register API for production

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -33,7 +33,7 @@ function Register() {
       setLoading(true);
 
       const response = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        `${import.meta.env.VITE_API_URL}/api/auth/register`,
         {
           username,
           email,
